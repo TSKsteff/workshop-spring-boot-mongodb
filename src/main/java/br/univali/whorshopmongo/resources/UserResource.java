bd795fc9eab221750b9e615354dc8199834d98f8
TSKsteff <steff@edu.univali.br>
2023-12-17 21:59:06 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/br/univali/whorshopmongo/resources/UserResource.java
+++ src/main/java/br/univali/whorshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.univali.whorshopmongo.domain.Post;
 import br.univali.whorshopmongo.domain.User;
 import br.univali.whorshopmongo.dto.UserDTO;
 import br.univali.whorshopmongo.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		user.setId(id);
 		user = userService.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
